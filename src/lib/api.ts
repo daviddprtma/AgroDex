416dea5b59f15e5734b5fcc67f72b4da780a7ea3
daviddprtma <davidchristianpratama@gmail.com>
2025-11-14 19:33:38 +0700

    [UPDATE] cors issue 16

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -312,7 +312,7 @@ export const getDashboardHealth = async () => {
   const headers: Record<string, string> = {
     Accept: "application/json",
     // handle cors
-    "Access-Control-Allow-Origin": "https://agro-dex-6sp2.vercel.app",
+    "Access-Control-Allow-Origin": "https://agro-dex-psi.vercel.app",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Credentials": "true",
